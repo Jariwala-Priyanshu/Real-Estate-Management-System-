cf25a04b9670a8b4e51dce0ae4a30976691e46b2
Jariwala-Priyanshu <163970321+Jariwala-Priyanshu@users.noreply.github.com>
2024-05-27 18:18:37 +0530

    Update propertyadd.php

--- admin/propertyadd.php
+++ admin/propertyadd.php
@@ -8,8 +8,7 @@ if(!isset($_SESSION['auser']))
 	header("location:index.php");
 }
 
-//// code insert
-//// add code
+
 $error="";
 $msg="";
 if(isset($_POST['add']))
@@ -89,36 +88,31 @@ if(isset($_POST['add']))
 <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
-        <title>LM HOMES | Property</title>
+        <title>JP HOMES | Property</title>
 		
-		<!-- Favicon -->
+		
         <link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.png">
 		
-		<!-- Bootstrap CSS -->
+		
         <link rel="stylesheet" href="assets/css/bootstrap.min.css">
 		
-		<!-- Fontawesome CSS -->
+		
         <link rel="stylesheet" href="assets/css/font-awesome.min.css">
 		
-		<!-- Feathericon CSS -->
+		
         <link rel="stylesheet" href="assets/css/feathericon.min.css">
 		
-		<!-- Main CSS -->
+		
         <link rel="stylesheet" href="assets/css/style.css">
 		
-		<!--[if lt IE 9]>
-			<script src="assets/js/html5shiv.min.js"></script>
-			<script src="assets/js/respond.min.js"></script>
-		<![endif]-->
+		
     </head>
     <body>
 
 		
-			<!-- Header -->
-			<?php include("header.php"); ?>
-			<!-- /Sidebar -->
 			
-			<!-- Page Wrapper -->
+			<?php include("header.php"); ?>
+			
             <div class="page-wrapper">
                 <div class="content container-fluid">
 				
@@ -134,7 +128,6 @@ if(isset($_POST['add']))
 							</div>
 						</div>
 					</div>
-					<!-- /Page Header -->
 					
 					<div class="row">
 						<div class="col-md-12">
@@ -455,21 +448,21 @@ if(isset($_POST['add']))
 				
 				</div>			
 			</div>
-			<!-- /Main Wrapper -->
+			
 
 		
-		<!-- jQuery -->
+		
         <script src="assets/js/jquery-3.2.1.min.js"></script>
 		<script src="assets/plugins/tinymce/tinymce.min.js"></script>
 		<script src="assets/plugins/tinymce/init-tinymce.min.js"></script>
-		<!-- Bootstrap Core JS -->
+		
         <script src="assets/js/popper.min.js"></script>
         <script src="assets/js/bootstrap.min.js"></script>
 		
-		<!-- Slimscroll JS -->
+	
         <script src="assets/plugins/slimscroll/jquery.slimscroll.min.js"></script>
 		
-		<!-- Custom JS -->
+		
 		<script  src="assets/js/script.js"></script>
 		
     </body>
