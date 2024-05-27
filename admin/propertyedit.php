23b1d8b9816919231ee942562fc36b6f7e5f899c
Jariwala-Priyanshu <163970321+Jariwala-Priyanshu@users.noreply.github.com>
2024-05-27 18:20:53 +0530

    Update propertyedit.php

--- admin/propertyedit.php
+++ admin/propertyedit.php
@@ -94,7 +94,7 @@ if(isset($_POST['add']))
 <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
-        <title>LM HOMES | Property</title>
+        <title>JP HOMES | Property</title>
 		
 		
         <link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.png">
