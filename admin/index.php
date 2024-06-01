5bbaa019833f9c3b52951e6eb0e4b556f8d77a5d
Jariwala-Priyanshu <163970321+Jariwala-Priyanshu@users.noreply.github.com>
2024-06-01 12:02:36 +0530

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -37,27 +37,24 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
         <title>RE Admin - Login</title>
 		
-		<!-- Favicon -->
+		
         <link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.png">
 
-		<!-- Bootstrap CSS -->
+		
         <link rel="stylesheet" href="assets/css/bootstrap.min.css">
 		
-		<!-- Fontawesome CSS -->
+		
         <link rel="stylesheet" href="assets/css/font-awesome.min.css">
 		
-		<!-- Main CSS -->
+		
         <link rel="stylesheet" href="assets/css/style.css">
 		
-		<!--[if lt IE 9]>
-			<script src="assets/js/html5shiv.min.js"></script>
-			<script src="assets/js/respond.min.js"></script>
-		<![endif]-->
+		
 		
     </head>
     <body>
 	
-		<!-- Main Wrapper -->
+	
         <div class="page-wrappers login-body">
             <div class="login-wrapper">
             	<div class="container">
@@ -67,7 +64,7 @@
 								<h1>Admin Login Panel</h1>
 								<p class="account-subtitle">Access to our dashboard</p>
 								<p style="color:red;"><?php echo $error; ?></p>
-								<!-- Form -->
+								
 								<form method="post">
 									<div class="form-group">
 										<input class="form-control" name="user" type="text" placeholder="User Name">
@@ -87,15 +84,13 @@
                 </div>
             </div>
         </div>
-		<!-- /Main Wrapper -->
 		
-		<!-- jQuery -->
         <script src="assets/js/jquery-3.2.1.min.js"></script>
-		<!-- Bootstrap Core JS -->
+		
         <script src="assets/js/popper.min.js"></script>
         <script src="assets/js/bootstrap.min.js"></script>
 		
-		<!-- Custom JS -->
+		
 		<script src="assets/js/script.js"></script>
 		
     </body>
