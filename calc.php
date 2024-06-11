8df10e9a8f5ef9abc17a78edf0769ac7840b144f
Jariwala-Priyanshu <priyanshu.jariwala001@gmail.com>
2024-06-11 14:54:44 +0530

    update file

--- calc.php
+++ calc.php
@@ -4,7 +4,7 @@ session_cache_limiter(false);
 session_start();
 include("config.php");
 
-// codeing
+
 
 if(isset($_REQUEST['calc']))
 {
@@ -22,22 +22,20 @@ if(isset($_REQUEST['calc']))
 <html lang="en">
 
 <head>
-<!-- Required meta tags -->
+
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-<!-- Meta Tags -->
+
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <link rel="shortcut icon" href="images/favicon.ico">
 
-<!--	Fonts
-	========================================================-->
+
 <link href="https://fonts.googleapis.com/css?family=Muli:400,400i,500,600,700&amp;display=swap" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css?family=Comfortaa:400,700" rel="stylesheet">
 
-<!--	Css Link
-	========================================================-->
+
 <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
 <link rel="stylesheet" type="text/css" href="css/bootstrap-slider.css">
 <link rel="stylesheet" type="text/css" href="css/jquery-ui.css">
@@ -48,53 +46,19 @@ if(isset($_REQUEST['calc']))
 <link rel="stylesheet" type="text/css" href="fonts/flaticon/flaticon.css">
 <link rel="stylesheet" type="text/css" href="css/style.css">
 <link rel="stylesheet" type="text/css" href="css/login.css">
-<!-- FOR MORE PROJECTS visit: codeastro.com -->
-<!--	Title
-	=========================================================-->
+
 <title>Real Estate PHP</title>
 </head>
 <body>
 
-<!--	Page Loader
-=============================================================
-<div class="page-loader position-fixed z-index-9999 w-100 bg-white vh-100">
-	<div class="d-flex justify-content-center y-middle position-relative">
-	  <div class="spinner-border" role="status">
-		<span class="sr-only">Loading...</span>
-	  </div>
-	</div>
-</div>
---> 
+
 
 
 <div id="page-wrapper">
     <div class="row"> 
-        <!--	Header start  -->
-		<?php include("include/header.php");?>
-        <!--	Header end  -->
         
-        <!--	Banner   --->
-        <!-- <div class="banner-full-row page-banner" style="background-image:url('images/breadcromb.jpg');">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-6">
-                        <h2 class="page-name float-left text-white text-uppercase mt-1 mb-0"><b>User Listed Property</b></h2>
-                    </div>
-                    <div class="col-md-6">
-                        <nav aria-label="breadcrumb" class="float-left float-md-right">
-                            <ol class="breadcrumb bg-transparent m-0 p-0">
-                                <li class="breadcrumb-item text-white"><a href="#">Home</a></li>
-                                <li class="breadcrumb-item active">User Listed Property</li>
-                            </ol>
-                        </nav>
-                    </div>
-                </div>
-            </div>
-        </div> -->
-         <!--	Banner   --->
-		 
-		 
-		<!--	Submit property   -->
+		<?php include("include/header.php");?>
+        
         <div class="full-row bg-gray">
             <div class="container">
                     <div class="row mb-5">
@@ -120,7 +84,7 @@ if(isset($_REQUEST['calc']))
 							<tr class="text-center">
                                 <td><b>Total Duration</b></td>
                                 <td><b><?php echo $mon.' Months' ; ?></b></td>
-                            </tr><!-- FOR MORE PROJECTS visit: codeastro.com -->
+                            </tr>
 							<tr class="text-center">
                                 <td><b>Interest Rate</b></td>
                                 <td><b><?php echo $int.'%' ; ?></b></td>
@@ -142,29 +106,20 @@ if(isset($_REQUEST['calc']))
                     </table> 
 					</center>
             </div>
-        </div><!-- FOR MORE PROJECTS visit: codeastro.com -->
-	<!--	Submit property   -->
-        
-        
-        <!--	Footer   start-->
+        </div>
 		<?php include("include/footer.php");?>
-		<!--	Footer   start-->
-        
-        <!-- Scroll to top --> 
+	
         <a href="#" class="bg-secondary text-white hover-text-secondary" id="scroll"><i class="fas fa-angle-up"></i></a> 
-        <!-- End Scroll To top --> 
+       
     </div>
 </div>
-<!-- Wrapper End --> 
 
-<!--	Js Link
-============================================================--> 
 <script src="js/jquery.min.js"></script> 
-<!--jQuery Layer Slider --> 
+
 <script src="js/greensock.js"></script> 
 <script src="js/layerslider.transitions.js"></script> 
 <script src="js/layerslider.kreaturamedia.jquery.js"></script> 
-<!--jQuery Layer Slider --> 
+
 <script src="js/popper.min.js"></script> 
 <script src="js/bootstrap.min.js"></script> 
 <script src="js/owl.carousel.min.js"></script> 
