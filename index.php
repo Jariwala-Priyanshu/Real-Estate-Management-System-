0c6fb01d0421b4953399d5472ca561167bec6c1e
Jariwala-Priyanshu <priyanshu.jariwala001@gmail.com>
2024-06-11 12:57:32 +0530

    update file

--- index.php
+++ index.php
@@ -9,23 +9,20 @@ include("config.php");
 <html lang="en">
 
 <head>
-<!-- FOR MORE PROJECTS visit: codeastro.com -->
-<!-- Required meta tags -->
+
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-<!-- Meta Tags -->
+
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <link rel="shortcut icon" href="images/favicon.ico">
 
-<!--	Fonts
-	========================================================-->
+	
 <link href="https://fonts.googleapis.com/css?family=Muli:400,400i,500,600,700&amp;display=swap" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css?family=Comfortaa:400,700" rel="stylesheet">
 
-<!--	Css Link
-	========================================================-->
+
 <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
 <link rel="stylesheet" type="text/css" href="css/bootstrap-slider.css">
 <link rel="stylesheet" type="text/css" href="css/jquery-ui.css">
@@ -36,29 +33,20 @@ include("config.php");
 <link rel="stylesheet" type="text/css" href="fonts/flaticon/flaticon.css">
 <link rel="stylesheet" type="text/css" href="css/style.css">
 
-<!--	Title
-	=========================================================-->
+
 <title>Real Estate PHP</title>
 </head>
 <body>
 
-<!--	Page Loader  -->
-<!--<div class="page-loader position-fixed z-index-9999 w-100 bg-white vh-100">
-	<div class="d-flex justify-content-center y-middle position-relative">
-	  <div class="spinner-border" role="status">
-		<span class="sr-only">Loading...</span>
-	  </div>
-	</div>
-</div>  -->
-<!--	Page Loader  -->
+
 
 <div id="page-wrapper">
     <div class="row"> 
-        <!--	Header start  -->
+        
 		<?php include("include/header.php");?>
-        <!--	Header end  -->
+        
 		
-        <!--	Banner Start   -->
+        
         <div class="overlay-black w-100 slider-banner1 position-relative" style="background-image: url('images/banner/3.jpg'); background-size: cover; background-position: center center; background-repeat: no-repeat;">
             <div class="container h-100">
                 <div class="row h-100 align-items-center">
@@ -107,14 +95,12 @@ include("config.php");
                 </div>
             </div>
         </div>
-        <!--	Banner End  -->
         
-        <!--	Text Block One
-		======================================================-->
+		
         <div class="full-row bg-gray">
             <div class="container">
                 <div class="row">
-                    <div class="col-lg-12"><!-- FOR MORE PROJECTS visit: codeastro.com -->
+                    <div class="col-lg-12">
                         <h2 class="text-secondary double-down-line text-center mb-5">What We Do</h2></div>
                 </div>
                 <div class="text-box-one">
@@ -151,24 +137,15 @@ include("config.php");
                 </div>
             </div>
         </div>
-		<!-----  Our Services  ---->
 		
-        <!--	Recent Properties  -->
         <div class="full-row">
             <div class="container">
                 <div class="row">
                     <div class="col-md-12">
                         <h2 class="text-secondary double-down-line text-center mb-4">Recent Property</h2>
                     </div>
-                    <!--- <div class="col-md-6">
-                        <ul class="nav property-btn float-right" id="pills-tab" role="tablist">
-                            <li class="nav-item"> <a class="nav-link py-3 active" id="pills-home-tab" data-toggle="pill" href="#pills-home" role="tab" aria-controls="pills-home" aria-selected="true">New</a> </li>
-                            <li class="nav-item"> <a class="nav-link py-3" id="pills-profile-tab" data-toggle="pill" href="#pills-profile" role="tab" aria-controls="pills-profile" aria-selected="false">Featured</a> </li>
-                            <li class="nav-item"> <a class="nav-link py-3" id="pills-contact-tab2" data-toggle="pill" href="#pills-contact" role="tab" aria-controls="pills-contact" aria-selected="false">Top Sale</a> </li>
-                            <li class="nav-item"> <a class="nav-link py-3" id="pills-contact-tab3" data-toggle="pill" href="#pills-resturant" role="tab" aria-controls="pills-contact" aria-selected="false">Best Sale</a> </li>
-                        </ul>
-                    </div> --->
-                    <div class="col-md-12"><!-- FOR MORE PROJECTS visit: codeastro.com -->
+                   
+                    <div class="col-md-12">
                         <div class="tab-content mt-4" id="pills-tabContent">
                             <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home">
                                 <div class="row">
@@ -218,9 +195,7 @@ include("config.php");
                 </div>
             </div>
         </div>
-		<!--	Recent Properties  -->
-        
-        <!--	Why Choose Us -->
+		
         <div class="full-row living bg-one overlay-secondary-half" style="background-image: url('images/02.jpg'); background-size: cover; background-position: center center; background-repeat: no-repeat;">
             <div class="container">
                 <div class="row">
@@ -255,9 +230,7 @@ include("config.php");
                 </div>
             </div>
         </div>
-		<!--	why choose us -->
 		
-		<!--	How it work -->
         <div class="full-row">
             <div class="container">
                 <div class="row">
@@ -294,10 +267,7 @@ include("config.php");
             </div>
         </div>
         
-        <!--	How It Work -->
         
-        <!--	Achievement
-        ============================================================-->
         <div class="full-row overlay-secondary" style="background-image: url('images/breadcromb.jpg'); background-size: cover; background-position: center center; background-repeat: no-repeat;">
             <div class="container">
                 <div class="fact-counter">
@@ -364,7 +334,7 @@ include("config.php");
             </div>
         </div>
         
-        <!--	Popular Place -->
+       
         <div class="full-row bg-gray">
             <div class="container">
                 <div class="row">
@@ -437,9 +407,7 @@ include("config.php");
                 </div>
             </div>
         </div>
-        <!--	Popular Places -->
-		
-		<!--	Testonomial -->
+        
 		<div class="full-row">
             <div class="container">
                 <div class="row">
@@ -472,29 +440,22 @@ include("config.php");
 				</div>
 			</div>
 		</div>
-		<!--	Testonomial -->
 		
-		
-        <!--	Footer   start-->
 		<?php include("include/footer.php");?>
-		<!--	Footer   start-->
-        
-        
-        <!-- Scroll to top --> 
+	 
         <a href="#" class="bg-success text-white hover-text-secondary" id="scroll"><i class="fas fa-angle-up"></i></a> 
-        <!-- End Scroll To top --> 
+      
     </div>
 </div>
-<!-- Wrapper End --> 
 
-<!--	Js Link
-============================================================--> 
+
+
 <script src="js/jquery.min.js"></script> 
-<!--jQuery Layer Slider --> 
+
 <script src="js/greensock.js"></script> 
 <script src="js/layerslider.transitions.js"></script> 
 <script src="js/layerslider.kreaturamedia.jquery.js"></script> 
-<!--jQuery Layer Slider --> 
+
 <script src="js/popper.min.js"></script> 
 <script src="js/bootstrap.min.js"></script> 
 <script src="js/owl.carousel.min.js"></script> 
