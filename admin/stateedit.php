2e2ca6ffcae40a03b36b127e0d635637b1f0bb9d
Jariwala-Priyanshu <priyanshu.jariwala001@gmail.com>
2024-06-11 10:39:46 +0530

    update file

--- admin/stateedit.php
+++ admin/stateedit.php
@@ -5,7 +5,7 @@ if(!isset($_SESSION['auser']))
 {
 	header("location:index.php");
 }
-///code
+
 $error="";
 $msg="";
 if(isset($_POST['insert']))
@@ -35,46 +35,41 @@ if(isset($_POST['insert']))
         <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
         <title>Ventura - Data Tables</title>
 		
-		<!-- Favicon -->
+		
         <link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.png">
 		
-		<!-- Bootstrap CSS -->
+		
         <link rel="stylesheet" href="assets/css/bootstrap.min.css">
 		
-		<!-- Fontawesome CSS -->
+		
         <link rel="stylesheet" href="assets/css/font-awesome.min.css">
 		
-		<!-- Feathericon CSS -->
+		
         <link rel="stylesheet" href="assets/css/feathericon.min.css">
 		
-		<!-- Datatables CSS -->
+		
 		<link rel="stylesheet" href="assets/plugins/datatables/dataTables.bootstrap4.min.css">
 		<link rel="stylesheet" href="assets/plugins/datatables/responsive.bootstrap4.min.css">
 		<link rel="stylesheet" href="assets/plugins/datatables/select.bootstrap4.min.css">
 		<link rel="stylesheet" href="assets/plugins/datatables/buttons.bootstrap4.min.css">
 		
-		<!-- Main CSS -->
+		
         <link rel="stylesheet" href="assets/css/style.css">
 		
-		<!--[if lt IE 9]>
-			<script src="assets/js/html5shiv.min.js"></script>
-			<script src="assets/js/respond.min.js"></script>
-		<![endif]-->
+		
     </head>
     <body>
 	
-		<!-- Main Wrapper -->
+		
 
 		
-			<!-- Header -->
-			<?php include("header.php");?>	
-			<!-- /Sidebar -->
 			
-			<!-- Page Wrapper -->
+			<?php include("header.php");?>	
+			
             <div class="page-wrapper">
                 <div class="content container-fluid">
 
-					<!-- Page Header -->
+				
 					<div class="page-header">
 						<div class="row">
 							<div class="col">
@@ -86,9 +81,7 @@ if(isset($_POST['insert']))
 							</div>
 						</div>
 					</div>
-					<!-- /Page Header -->
-					
-				<!-- state add section --> 
+				
 					<div class="row">
 						<div class="col-md-12">
 							<div class="card">
@@ -125,23 +118,20 @@ if(isset($_POST['insert']))
 							</div>
 						</div>
 					</div>
-				<!----End state add section  --->
+				
 				</div>			
 			</div>
-			<!-- /Main Wrapper -->
-
-		<!-- jQuery -->
+		
         <script src="assets/js/jquery-3.2.1.min.js"></script>
 		
-		<!-- Bootstrap Core JS -->
+		
         <script src="assets/js/popper.min.js"></script>
         <script src="assets/js/bootstrap.min.js"></script>
 		
-		<!-- Slimscroll JS -->
+		
         <script src="assets/plugins/slimscroll/jquery.slimscroll.min.js"></script>
 		
-		<!-- Datatables JS -->
-		<!-- Datatables JS -->
+	
 		<script src="assets/plugins/datatables/jquery.dataTables.min.js"></script>
 		<script src="assets/plugins/datatables/dataTables.bootstrap4.min.js"></script>
 		<script src="assets/plugins/datatables/dataTables.responsive.min.js"></script>
@@ -155,7 +145,7 @@ if(isset($_POST['insert']))
 		<script src="assets/plugins/datatables/buttons.flash.min.js"></script>
 		<script src="assets/plugins/datatables/buttons.print.min.js"></script>
 		
-		<!-- Custom JS -->
+	
 		<script  src="assets/js/script.js"></script>
 		
     </body>
