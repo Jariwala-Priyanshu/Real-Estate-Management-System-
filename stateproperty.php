a28b258600d106624190f1d474b7478c7fa7ad72
Jariwala-Priyanshu <priyanshu.jariwala001@gmail.com>
2024-06-11 16:33:46 +0530

    update file

--- stateproperty.php
+++ stateproperty.php
@@ -150,19 +150,16 @@ include("config.php");
         
         
         <a href="#" class="bg-secondary text-white hover-text-secondary" id="scroll"><i class="fas fa-angle-up"></i></a> 
-        <!-- End Scroll To top --> 
+      
     </div>
 </div>
-<!-- Wrapper End --> 
 
-<!--	Js Link
-============================================================--> 
 <script src="js/jquery.min.js"></script> 
-<!--jQuery Layer Slider --> 
+
 <script src="js/greensock.js"></script> 
 <script src="js/layerslider.transitions.js"></script> 
 <script src="js/layerslider.kreaturamedia.jquery.js"></script> 
-<!--jQuery Layer Slider --> 
+
 <script src="js/popper.min.js"></script> 
 <script src="js/bootstrap.min.js"></script> 
 <script src="js/owl.carousel.min.js"></script> 
